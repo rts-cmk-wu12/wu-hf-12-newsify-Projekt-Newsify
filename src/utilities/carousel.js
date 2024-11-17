23765b9e92e5f501a0f6b44694831e291d4b5e3e
milotobias <tobiastap@gmail.com>
2024-11-17 20:45:37 +0100

    annoying problem has occured with styling of image

--- src/utilities/carousel.js
+++ src/utilities/carousel.js
@@ -1,5 +1,3 @@
 import Carousel from '../classes/carouselClass';
 
-document.addEventListener('DOMContentLoaded', () => {
-    new Carousel('#carousel', '#carousel__slides', '#carousel__dots', '#carousel__next');
-});
+new Carousel('#carousel', '#carousel__slides', '#carousel__dots', '#carousel__next');
